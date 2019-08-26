1a606e1818f6aceb6027e169df6f84914b50e436
Aliaksandr Daineka <aliaksandr_daineka@epam.com>
2019-08-26 19:34:59 +0300

    Complete implementation of Service and Repository GetCourses

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
